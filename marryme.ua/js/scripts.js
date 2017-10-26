10730cb42ddd28ed5defa7372daceb23539cff8b
MCodeDesigner <mcode.designer@gmail.com>
2017-10-26 08:55:38 +0300

    commit

---
+++ marryme.ua/js/scripts.js
@@ -0,0 +1,13 @@
+$('#gallary-init').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+$('#photo-init').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
