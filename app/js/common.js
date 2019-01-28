e61916aee803039445b7308fe30467e88867be1a
MCodeDesigner <mcode.designer@gmail.com>
2019-01-29 01:10:12 +0200

    234567

---
+++ app/js/common.js
@@ -0,0 +1,21 @@
+$(function() {
+
+	// Mobile Menu
+	$(".sandwich-box").click(function() {
+	  $(".sandwich-icon").toggleClass("active");
+	});
+	$(".main-menu li a").click(function() {
+		$(".main-menu").fadeOut(600);
+		$(".sandwich-icon").toggleClass("active");
+	});
+	$(".sandwich-box").click(function() {
+		if($(".main-menu").is(":visible")) {
+			$(".main-menu").fadeOut(600);
+			$(".main-menu li").removeClass("fadeInUp animated");
+		} else {
+			$(".main-menu").fadeIn(600);
+			$(".main-menu li").addClass("fadeInUp animated");
+		};
+	});
+
+});
