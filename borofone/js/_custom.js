c44e92c961e4a52e624c33904eaa0da82eb68105
MCodeDesigner <mcode.designer@gmail.com>
2019-10-01 18:43:12 +0300

    3456789щ

--- borofone/js/_custom.js
+++ borofone/js/_custom.js
@@ -7,8 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
 	// initialized owlSlider
-	var owlBrand = $('.owl-brand');
-	owlBrand.owlCarousel({
+	$('.owl-brand').owlCarousel({
 		loop: true,
 		autoWidth:true,
 	    items:1,
@@ -22,8 +21,58 @@ document.addEventListener("DOMContentLoaded", function() {
 		autoplay: true,
 		autoplayTimeout: 5000,
 		autoplayHoverPause: true,
-		smartSpeed: 1500
+		smartSpeed: 1000
 	});
-	// end initialized owlSlider+
+
+	// initialized owlSlider
+	$('#owl-review__partners').owlCarousel({
+		loop: true,
+		// autoWidth: true,
+	    items:4,
+		margin: 0,
+		nav: true,
+		navText: [
+			"<span class='owl-btn'><i class='i-left'></i></span>",
+			"<span class='owl-btn'><i class='i-right'></i></span>"
+		],
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		smartSpeed: 1000
+	});
+
+	// initialized owlSlider
+	$('#owl-catalog').owlCarousel({
+		loop: true,
+		// autoWidth: true,
+	    items:4,
+		margin: 0,
+		nav: true,
+		navText: [
+			"<span class='owl-btn'><i class='i-left'></i></span>",
+			"<span class='owl-btn'><i class='i-right'></i></span>"
+		],
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		smartSpeed: 1000
+	});
+
+
+	// initialized tabs
+	var m = new Tabs({
+		el: "#our-product--tabs",
+		marker: true
+	});
+	m.init();
+
+
+	$('.review__description-1').expandable({
+	    height: 135,
+	    more: 'Показать полностью',
+	    less: 'Свернуть'
+	});
 
 });
